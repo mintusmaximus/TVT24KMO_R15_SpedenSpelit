e341c0bd8f75ce023a3429616ec272b78ecd1939
Jarmoit <jmarjakangas@gmail.com>
2024-09-26 16:43:53 +0300

    Tunnari ja nappulaäänet

--- SpedenSpelit/sound.cpp
+++ SpedenSpelit/sound.cpp
@@ -1,7 +1,13 @@
+/* Jarmo : Mun mielestä nyt riittää starttiääni, satunnaisgeneraattorilla valitun painettavan
+napin "käynnistysääni", ja game over.*/
+
 #include "sound.h"
 #include "pitches.h"
 
 const int buzzerPin = 7; // jos rakennetaan projektisuunnitelman mukaan, pinni 7 pitäisi olla vapaa
+int startsound[] = {
+  NOTE_F4, NOTE_A4, NOTE_F5, NOTE_E5, NOTE_F5, NOTE_F6, NOTE_F7
+};
 
 
 // Sound setup for the game
@@ -16,8 +22,41 @@ void initializeSound(void)
 // Sound for the start of the game
 void playGameStartSound(void)
 {
-
+    for (int i = 0; i < 8; i++){
+        tone(buzzerpin, startsound[i], 200)
+        delay(300);
+        noTone(buzzerpin);
+    }
+    
 }
+void playButtonOneSelected(void)
+{
+    tone(buzzerpin, NOTE_F5, 150)
+    delay(150);
+    noTone(buzzerpin);
+}
+
+void playButtonTwoSelected(void)
+{
+    tone(buzzerpin, NOTE_D5, 150)
+    delay(150);
+    noTone(buzzerpin);
+}
+
+void playButtonThreeSelected(void)
+{
+    tone(buzzerpin, NOTE_B4, 150)
+    delay(150);
+    noTone(buzzerpin);
+}
+
+void playButtonFourSelected(void)
+{
+    tone(buzzerpin, NOTE_G4, 150)
+    delay(150);
+    noTone(buzzerpin);
+}
+
 
 
 // Sound for the end of the game
