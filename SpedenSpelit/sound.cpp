2ff1445dcbc47b9ac0e00eb983fa29b9ee813b36
mintusmaximus <156218528+mintusmaximus@users.noreply.github.com>
2024-09-26 14:52:03 +0300

    Add sound effect for wrong answer

--- SpedenSpelit/sound.cpp
+++ SpedenSpelit/sound.cpp
@@ -37,7 +37,11 @@ void playCorrectSound(void)
 // Sound for a wrong answer
 void playWrongSound(void)
 {
-
+    Serial.println("SOUND: Playing wrong answer");
+    for (int i = 1000; i >= 500; i -= 500){ // Play a descending tone from 1000 to 500 Hz 
+        tone(buzzerPin, i, 200); // Play the tone for 200 ms 
+        delay(100); // Wait for 100 ms 
+    }
 }
 
 
